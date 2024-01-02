9c5aec59997ae78dc6a79b4573a2077dfa4bc80b
cheezi0747 <linus.sjolinder@b3.se>
2024-01-02 11:50:20 +0100

    new endpoint to trigger batchjob

--- src/main/java/se/sundsvall/snailmail/api/SnailMailResource.java
+++ src/main/java/se/sundsvall/snailmail/api/SnailMailResource.java
@@ -3,14 +3,18 @@ package se.sundsvall.snailmail.api;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
 
+import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zalando.problem.Problem;
 
+import se.sundsvall.dept44.common.validators.annotation.ValidUuid;
 import se.sundsvall.snailmail.api.model.SendSnailMailRequest;
 import se.sundsvall.snailmail.service.SnailMailService;
 
@@ -19,12 +23,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 
 @RestController
 @Validated
 @RequestMapping("/send/snailmail")
 @Tag(name = "SnailMailSender", description = "SnailMailSender")
+@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Problem.class)))
+@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = Problem.class)))
 public class SnailMailResource {
 
 	private final SnailMailService snailMailService;
@@ -36,11 +41,18 @@ public class SnailMailResource {
 	@PostMapping(consumes = APPLICATION_JSON_VALUE)
 	@Operation(summary = "Create snailmail")
 	@ApiResponse(responseCode = "200", description = "Successful Operation", useReturnTypeSchema = true)
-	@ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(schema = @Schema(implementation = Problem.class)))
-	@ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(schema = @Schema(implementation = Problem.class)))
-	ResponseEntity<Void> sendSnailMail(@Valid @RequestBody SendSnailMailRequest request) {
+	ResponseEntity<Void> sendSnailMail(@Valid @RequestBody final SendSnailMailRequest request) {
 
 		snailMailService.sendSnailMail(request);
 		return ok().build();
 	}
+
+	@PostMapping("batch/{batchId}")
+	@Operation(summary = "Send batch")
+	@ApiResponse(responseCode = "200", description = "Successful Operation", useReturnTypeSchema = true)
+	ResponseEntity<Void> sendBatch(@ValidUuid @PathVariable final String batchId) {
+		snailMailService.sendBatch(batchId);
+		return ok().build();
+	}
+
 }
--- src/test/java/se/sundsvall/snailmail/api/SnailMailResourceTest.java
+++ src/test/java/se/sundsvall/snailmail/api/SnailMailResourceTest.java
@@ -1,39 +1,100 @@
 package se.sundsvall.snailmail.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static se.sundsvall.snailmail.TestDataFactory.buildSendSnailMailRequest;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.zalando.problem.Status.BAD_REQUEST;
+
+import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.zalando.problem.violations.ConstraintViolationProblem;
+import org.zalando.problem.violations.Violation;
 
+import se.sundsvall.snailmail.Application;
 import se.sundsvall.snailmail.api.model.SendSnailMailRequest;
 import se.sundsvall.snailmail.service.SnailMailService;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest(classes = Application.class, webEnvironment = RANDOM_PORT)
+@ActiveProfiles("junit")
 class SnailMailResourceTest {
 
-	@Mock
+	@MockBean
 	private SnailMailService mockSnailMailService;
 
-	@InjectMocks
-	private SnailMailResource snailMailResource;
+
+	@Autowired
+	private WebTestClient webTestClient;
+
 
 	@Test
 	void sendSnailMail() {
-		doNothing().when(mockSnailMailService).sendSnailMail(any(SendSnailMailRequest.class));
 
-		final var response = snailMailResource.sendSnailMail(buildSendSnailMailRequest());
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		//ACT
+		webTestClient.post()
+			.uri("/send/snailmail")
+			.bodyValue(SendSnailMailRequest.builder()
+				.withDepartment("department")
+				.build())
+			.exchange()
+			.expectStatus()
+			.isOk();
 
+		// VERIFY
 		verify(mockSnailMailService, times(1)).sendSnailMail(any(SendSnailMailRequest.class));
 
 	}
+
+
+	@Test
+	void sendBatch() {
+
+		final var uuid = UUID.randomUUID().toString();
+
+		//ACT
+		webTestClient.post()
+			.uri("/send/snailmail/batch/" + uuid)
+			.exchange()
+			.expectStatus()
+			.isOk();
+
+		verify(mockSnailMailService, times(1)).sendBatch(any(String.class));
+
+
+	}
+
+	@Test
+	void sendBatch_notUUID() {
+
+		//ACT
+		final var response = webTestClient.post()
+			.uri("/send/snailmail/batch/abc")
+			.exchange()
+			.expectStatus()
+			.isBadRequest()
+			.expectBody(ConstraintViolationProblem.class)
+			.returnResult()
+			.getResponseBody();
+
+		//ASSERT
+		assertThat(response).isNotNull();
+		assertThat(response.getTitle()).isEqualTo("Constraint Violation");
+		assertThat(response.getStatus()).isEqualTo(BAD_REQUEST);
+		assertThat(response.getViolations())
+			.extracting(Violation::getField, Violation::getMessage)
+			.containsExactlyInAnyOrder(tuple("sendBatch.batchId", "not a valid UUID"));
+
+		verifyNoInteractions(mockSnailMailService);
+
+	}
+
 }
