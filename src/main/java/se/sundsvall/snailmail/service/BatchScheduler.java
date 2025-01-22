79371f0653e7a29603d3a6ba204c188d629ad75f
cheezi0747 <linus.sjolinder@b3.se>
2025-01-22 13:08:04 +0100

    UF-11787: Use dept44-scheduler

--- src/main/java/se/sundsvall/snailmail/service/BatchScheduler.java
+++ src/main/java/se/sundsvall/snailmail/service/BatchScheduler.java
@@ -2,12 +2,11 @@ package se.sundsvall.snailmail.service;
 
 import java.time.Duration;
 import java.time.OffsetDateTime;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import se.sundsvall.dept44.requestid.RequestId;
+import se.sundsvall.dept44.scheduling.Dept44Scheduled;
 import se.sundsvall.snailmail.config.BatchProperties;
 
 @Component
@@ -18,24 +17,26 @@ public class BatchScheduler {
 	private final BatchProperties properties;
 	private final SnailMailService snailMailService;
 
-	public BatchScheduler(BatchProperties properties, SnailMailService snailMailService) {
+	public BatchScheduler(final BatchProperties properties, final SnailMailService snailMailService) {
 		this.snailMailService = snailMailService;
 		this.properties = properties;
 	}
 
 	/**
-	 * Check for batches that has not been triggered, for whatever reason.
-	 * It is outdated if the batch was created before the configured duration.
+	 * Check for batches that has not been triggered, for whatever reason. It is outdated if the batch was created before
+	 * the configured duration.
 	 */
-	@Scheduled(fixedRateString = "#{@batchProperties.getCheckInterval()}", initialDelayString = "#{@batchProperties.getInitialDelay()}")
-	@SchedulerLock(name = "#{@batchProperties.getName()}", lockAtMostFor = "#{@batchProperties.getLockAtMostFor()}")
+	@Dept44Scheduled(cron = "${batch.unhandled.cron}",
+		name = "${batch.unhandled.name}",
+		lockAtMostFor = "${batch.unhandled.lock-at-most-for}",
+		maximumExecutionTime = "${batch.unhandled.maximum-execution-time}")
 	public void sendUnhandledBatches() {
 		RequestId.init();
 		LOGGER.info("Checking for unhandled batches");
 
 		try {
-			var outdatedBefore = OffsetDateTime.now().minus(Duration.parse(properties.getOutdatedAfter()));
-			var outdatedBatches = snailMailService.getUnhandledBatches(outdatedBefore);
+			final var outdatedBefore = OffsetDateTime.now().minus(Duration.parse(properties.getOutdatedAfter()));
+			final var outdatedBatches = snailMailService.getUnhandledBatches(outdatedBefore);
 
 			if (!outdatedBatches.isEmpty()) {
 				LOGGER.info("Found {} unhandled batches, triggering them.", outdatedBatches.size());
