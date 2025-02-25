670a7be121bc2ca433463c8f5422c1bcaaf40185
AndreSoderlund <andre.soderlund@b3.se>
2025-02-25 10:12:38 +0100

    Added SFTP integration. Added properties to switch between Samba and SFTP

--- src/test/java/se/sundsvall/snailmail/service/SnailMailServiceTest.java
+++ src/test/java/se/sundsvall/snailmail/service/SnailMailServiceTest.java
@@ -269,6 +269,44 @@ class SnailMailServiceTest {
 		verifyNoInteractions(departmentRepositoryMock, requestRepositoryMock);
 	}
 
+	/**
+	 * Tests the scenario where sambaActive is false and sftpActive is true
+	 */
+	@Test
+	void sendBatch_2() {
+		var batchEntity = BatchEntity.builder().build();
+
+		when(batchRepositoryMock.findByMunicipalityIdAndId(MUNICIPALITY_ID, BATCH_ID)).thenReturn(Optional.ofNullable(batchEntity));
+		ReflectionTestUtils.setField(snailMailService, "sftpActive", true);
+
+		snailMailService.sendBatch(MUNICIPALITY_ID, BATCH_ID);
+
+		verify(batchRepositoryMock).findByMunicipalityIdAndId(MUNICIPALITY_ID, BATCH_ID);
+		verify(sftpIntegrationMock).writeBatchDataToSftp(any(BatchEntity.class));
+		verify(batchRepositoryMock).delete(any(BatchEntity.class));
+		verifyNoMoreInteractions(batchRepositoryMock, sambaIntegrationMock);
+		verifyNoInteractions(departmentRepositoryMock, requestRepositoryMock);
+	}
+
+	/**
+	 * Tests the scenario where sambaActive is false and sftpActive is false
+	 */
+	@Test
+	void sendBatch_3() {
+		var batchEntity = BatchEntity.builder().build();
+
+		when(batchRepositoryMock.findByMunicipalityIdAndId(MUNICIPALITY_ID, BATCH_ID)).thenReturn(Optional.ofNullable(batchEntity));
+
+		assertThatThrownBy(() -> snailMailService.sendBatch(MUNICIPALITY_ID, BATCH_ID))
+			.isInstanceOf(Problem.class)
+			.hasMessage("Internal Server Error: No integration active");
+
+		verify(batchRepositoryMock).findByMunicipalityIdAndId(MUNICIPALITY_ID, BATCH_ID);
+		verify(batchRepositoryMock, never()).delete(any(BatchEntity.class));
+		verifyNoMoreInteractions(batchRepositoryMock);
+		verifyNoInteractions(departmentRepositoryMock, requestRepositoryMock, sambaIntegrationMock);
+	}
+
 	@Test
 	void sendBatchWithNoBatchFound() {
 		when(batchRepositoryMock.findByMunicipalityIdAndId(MUNICIPALITY_ID, BATCH_ID)).thenReturn(Optional.empty());
