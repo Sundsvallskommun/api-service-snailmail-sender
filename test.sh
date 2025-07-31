699c8706cb3cd2ea3d6f195b5218ede5a6ce55a6
Michael Carlberg <michael.carlberg@b3.se>
2025-07-31 17:39:24 +0200

    Add -f flag to test.sh to force rebuild

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ set -xe
 
 cd test
 
+if [ "$1" = '-f' ]; then
+  docker compose down -v
+fi
+
 if docker compose ps app | grep -q '(healthy)'; then
   docker compose run --rm smoketest || docker compose logs app
 else
